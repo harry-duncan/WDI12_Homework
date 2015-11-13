ff3269b912dab438d8214b64b57ed456c7cd4455
Noviny BC <benjaminconolly@gmail.com>
2015-11-13 22:25:51 +1100

    Friday night end point

--- BenConolly/week_01/MTA_map/map.js
+++ BenConolly/week_01/MTA_map/map.js
@@ -1,38 +1,3 @@
-// var line = {
-// 	N_line: ["Times Square", "34th", "28th", "23rd", 
-// "Union Square", "8th"]
-// }
-
-
-
-// var specLine = function (thisLine) {
-// 	return line.thisLine[0];
-// };
-
-
-// 34th, N_line
-// var stationDiff = function(from, to) {
-// 	if ( stationPos(N_line, from) )
-// };
-
-
- 
-
-
-// var pickStation = function (thisLine, station) {
-// 	for (var i = 0; i < thisLine.length; i++) {
-// 		if (line[i] === station ) {
-// 			return station;
-// 		}
-// 	};
-// };
-
-
-
-
-
-
-
 
 
 var line = {
@@ -67,44 +32,98 @@ var line = {
 //===================================================
 
 //add a line variable for when we stop hard-coding the line
-var betweenStations = function (lineX, stationOne, stationTwo) {
+var stationsBetween = function (lineX, stationOne, stationTwo) {
 	// var line = N_line;
 	var onePos = line[lineX].indexOf(stationOne);
 	var twoPos = line[lineX].indexOf(stationTwo);
-	console.log(onePos);
-	console.log(twoPos);
 	var stationDiff = null;
 	var betweenStations = [];
-	// var stationDir = null;
-	// console.log(onePos);
-	// console.log(twoPos);
 	if (onePos > twoPos ) {
-		// stationDir = "backward"
 		stationDiff = onePos - twoPos;
-		for (var i = onePos ; i > twoPos ; i--) {
-			betweenStations.push(lineX[i])
-		}
+		// for (var i = onePos ; i > twoPos+1 ; i--) {
+		// 	betweenStations.push(lineX[i])
+		// }
+		//Replacing the above with the below single line
+		betweenStations = line[lineX].slice(onePos, twoPos)
+		console.log(betweenStations)
+		return betweenStations
 	} else if ( twoPos > onePos ) {
-		// stationDir = "forward"
 		stationDiff = twoPos - onePos;
-		for (var i = onePos ; i < twoPos ; i++) {
+		for (var i = onePos ; i < twoPos+1 ; i++) {
 			betweenStations.push(line[lineX][i])
 			console.log(betweenStations)
-			// console.log(stationDiff)
 
 		}
 	} else {
 		stationDiff = "Stations either not found, or the same station"
 	};
-	return console.log("You must travel " + stationDiff + " stations, passing " + 
-		betweenStations);
+	console.log("Between stations are " + betweenStations)
+	// return betweenStations
 }
+
+
+
+//1: add an if to check if the station you are trying to get to is not on your branch
+
+var planTrip = function (lineX, stationOne, lineY, station2) {
+	var allStations = [];
+	//It looks like the variable station2 is not being properly usedin the function
+	//I have no idea why it will run it for stationOne, but not station2
+			allStations.push((stationsBetween(lineX, "Union Square", station2)));
+
+	if ( lineX !== lineY ) {
+
+
+
+		// allStations.push((stationsBetween(lineX, stationOne, "Union Square")));
+		// //The below code does not run properly. Even if placed earlier in the statement.
+		// allStations.push((stationsBetween(lineY, "Union Square", stationTwo)));
+
+	} //else {
+	// 	stationsBetween(lineX, stationOne, stationTwo);
+	// }
+	console.log(allStations)
+}
+
+
+//==================================
+//Okay, what are we doing next?
+//1: add an if to check if the station you are trying to get to is not on your branch
+//2: go to union station, then switch branch go to the desired station
+
+
 
 
 
 //Work out adding slice/split
 //Eloquent javascript
 //Speaking javascript
+
+
+//schmancy it is to find the nearest cross path to chance at.
+//Hack method, theck for a station in both arrays in both directions.
+//Use that to 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
