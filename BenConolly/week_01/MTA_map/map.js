ccbd1835f4f5bb9d6862d5b21f4547f3712e7c75
Noviny BC <benjaminconolly@gmail.com>
2015-11-14 18:16:11 +1100

    MTA complete but still adding additional functionality

--- BenConolly/week_01/MTA_map/map.js
+++ BenConolly/week_01/MTA_map/map.js
@@ -4,8 +4,8 @@ var line = {
 	N: ["Times Square", "34th", "28th", "23rd", 
 	"Union Square", "8th"],
 	L: ["8th", "6th", "Union Square", "3rd", "1st"],
-	// "6": ["Grand Central", "33rd", "28th", "23rd", "Union Square",
-	// "Astor Place"],
+	"6": ["Grand Central", "33rd", "28th", "23rd", "Union Square",
+	"Astor Place"],
 };
 
 //I wrote this function,
@@ -32,57 +32,40 @@ var line = {
 //===================================================
 
 //add a line variable for when we stop hard-coding the line
-var stationsBetween = function (lineX, stationOne, stationTwo) {
+var logJourney = function (lineX, stationOne, stationTwo) {
 	// var line = N_line;
 	var onePos = line[lineX].indexOf(stationOne);
 	var twoPos = line[lineX].indexOf(stationTwo);
-	var stationDiff = null;
-	var betweenStations = [];
-	if (onePos > twoPos ) {
-		stationDiff = onePos - twoPos;
-		// for (var i = onePos ; i > twoPos+1 ; i--) {
-		// 	betweenStations.push(lineX[i])
-		// }
-		//Replacing the above with the below single line
-		betweenStations = line[lineX].slice(onePos, twoPos)
-		console.log(betweenStations)
-		return betweenStations
-	} else if ( twoPos > onePos ) {
-		stationDiff = twoPos - onePos;
-		for (var i = onePos ; i < twoPos+1 ; i++) {
-			betweenStations.push(line[lineX][i])
-			console.log(betweenStations)
+	// debugger;
+	var smallestIndex = Math.min( onePos, twoPos );
+	var biggestIndex = Math.max( onePos, twoPos );
+	var betweenStations = line[lineX].slice(smallestIndex, biggestIndex);
 
-		}
-	} else {
-		stationDiff = "Stations either not found, or the same station"
+	if (onePos > twoPos ) {
+		betweenStations = betweenStations.reverse();
 	};
-	console.log("Between stations are " + betweenStations)
-	// return betweenStations
+	if ( onePos < twoPos ) {
+		betweenStations = line[lineX].slice(smallestIndex+1, biggestIndex+1)
+	};
+	return betweenStations;
 }
 
 
-
-//1: add an if to check if the station you are trying to get to is not on your branch
-
 var planTrip = function (lineX, stationOne, lineY, station2) {
-	var allStations = [];
-	//It looks like the variable station2 is not being properly usedin the function
-	//I have no idea why it will run it for stationOne, but not station2
-			allStations.push((stationsBetween(lineX, "Union Square", station2)));
-
+	var allStations = ["empty"];
+	console.log(lineX, stationOne, lineY, station2)
 	if ( lineX !== lineY ) {
+		allStations = (logJourney(lineX, stationOne, "Union Square"));
+		console.log(allStations);
 
+		allStations =  allStations.concat((logJourney(lineY, "Union Square", station2)));
+		console.log(allStations);
 
-
-		// allStations.push((stationsBetween(lineX, stationOne, "Union Square")));
-		// //The below code does not run properly. Even if placed earlier in the statement.
-		// allStations.push((stationsBetween(lineY, "Union Square", stationTwo)));
-
-	} //else {
-	// 	stationsBetween(lineX, stationOne, stationTwo);
-	// }
-	console.log(allStations)
+	} else {
+		allStations = (logJourney(lineY, stationOne, station2));
+		console.log(allStations)
+	}
+	console.log ("You travelled " + allStations.length + " stops, passing through " + allStations.join(", "))
 }
 
 
@@ -105,23 +88,42 @@ var planTrip = function (lineX, stationOne, lineY, station2) {
 //Use that to 
 
 
+var theLines = Object.keys(line);
+
+
+
+
+var whatLines = function (station) {
+	var theLines = Object.keys(line);
+	// console.log(theLines);
+	for (var i = 0; i < theLines.length; i++) {
+		if (line [ theLines[i] ].indexOf(station) >= 0 ) {
+			//We need to use i to determine an object name
+			console.log (line[i]);
+		};
+		console.log(line[theLines])
+	};
+	console.log("Applicable")
+};
 
 
 
 
 
 
+// for ( var i = 0; i < objKeys.length; i++ ) {
+//   var objCurrentKey = objKeys[ i ];
+//   if ( obj[ objCurrentKey ] === "Female" ) {
+//     console.log( objCurrentKey );
+//   }
+// }
 
 
+//Object.Keys ( obj )
 
 
-
-
-
-
-
-
-
+// var objKeys = Object.keys( obj );
+undefined
 
 
 
