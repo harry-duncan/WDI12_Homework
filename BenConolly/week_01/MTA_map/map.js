d4a4788055fe0c3ff8eaec94e86d3068aa9108a5
Noviny BC <benjaminconolly@gmail.com>
2015-11-14 22:36:10 +1100

    All functions complete. logJourney2 can transfer you through Union square from only station names

--- BenConolly/week_01/MTA_map/map.js
+++ BenConolly/week_01/MTA_map/map.js
@@ -53,17 +53,17 @@ var logJourney = function (lineX, stationOne, stationTwo) {
 
 var planTrip = function (lineX, stationOne, lineY, station2) {
 	var allStations = ["empty"];
-	console.log(lineX, stationOne, lineY, station2)
+	// console.log(lineX, stationOne, lineY, station2)
 	if ( lineX !== lineY ) {
 		allStations = (logJourney(lineX, stationOne, "Union Square"));
-		console.log(allStations);
+		// console.log(allStations);
 
 		allStations =  allStations.concat((logJourney(lineY, "Union Square", station2)));
-		console.log(allStations);
+		// console.log(allStations);
 
 	} else {
 		allStations = (logJourney(lineY, stationOne, station2));
-		console.log(allStations)
+		// console.log(allStations)
 	}
 	console.log ("You travelled " + allStations.length + " stops, passing through " + allStations.join(", "))
 }
@@ -88,42 +88,123 @@ var planTrip = function (lineX, stationOne, lineY, station2) {
 //Use that to 
 
 
-var theLines = Object.keys(line);
-
-
+//=======================================
+//Beyond this point are non-necessary exra stuff.
+//The above functions are rewritten below.
+//This is to remove line references from the above, a station-only version.
 
 
 var whatLines = function (station) {
-	var theLines = Object.keys(line);
-	// console.log(theLines);
-	for (var i = 0; i < theLines.length; i++) {
-		if (line [ theLines[i] ].indexOf(station) >= 0 ) {
+	var listLines = Object.keys(line);
+	var theLines = []
+	// console.log(listLines);
+	for (var i = 0; i < listLines.length; i++) {
+		if (line [ listLines[i] ].indexOf(station) >= 0 ) {
 			//We need to use i to determine an object name
-			console.log (line[i]);
+			theLines.push(listLines[i]);
 		};
-		console.log(line[theLines])
 	};
-	console.log("Applicable")
+	// console.log("Forloop over")
+	return theLines
 };
 
 
+var checkSameLine = function (stationX, stationY) {
+	var statXLines = whatLines(stationX);
+	// debugger
+	var statYLines = whatLines(stationY);
+	var sharedLines = []
+	// console.log (statXLines)
+	// console.log (statYLines)
+	if (statXLines.length < statYLines.length) {
+		for (var i = 0; i < statYLines.length; i++) {
+			// console.log (statYlines.indexOf(statXLines[i]))
+			if (statXLines.indexOf(statYLines[i]) >= 0) {
+				sharedLines.push(statYLines[i]);
+			};
+		};
+	} else if (statXLines.length >= statYLines.length) {
+		for (var i = 0; i < statXLines.length; i++) {
+			// console.log (statYlines.indexOf(statXLines[i]))
+			if (statYLines.indexOf(statXLines[i]) >= 0) {
+				sharedLines.push(statXLines[i]);
+			};
+		};
+	};
+		return sharedLines
+};
+
+
+// Debug checksamelines weirdness where it can
+
+
+
+//=====================================
+//LogJourney 2 will take any two stations, and take you from one to the other via Union Square
+//Next step is to make them if on different lines find the closest transfer.
+//=====================================
+var logJourney2 = function ( stationOne, stationTwo) {
+	sharedLines = checkSameLine ( stationOne, stationTwo );
+	// console.log (sharedLines);
+	if ( sharedLines.length > 0 ) {
+		// console.log(sharedLines);
+		var onePos = line[sharedLines[0]].indexOf(stationOne);
+		var twoPos = line[sharedLines[0]].indexOf(stationTwo);
+		var smallestIndex = Math.min( onePos, twoPos );
+		var biggestIndex = Math.max( onePos, twoPos );
+		if (onePos > twoPos ) {
+			var betweenStations = sharedLines[0].slice(smallestIndex, biggestIndex)
+			betweenStations = betweenStations.reverse();
+		};
+		if ( onePos < twoPos ) {
+			var betweenStations = line[sharedLines[0]].slice(smallestIndex+1, biggestIndex+1)
+		};
+	};
+	if ( sharedLines.length <= 0 ) {
+		planTrip( whatLines(stationOne), stationOne, whatLines(stationTwo), stationTwo )
+
+	}
+		return betweenStations;
+	}
+
+
+//==============================
+//I feel like we are sooooo close to this working...
+var closestTransfer = function (station, line) {
+	
+}
+// Start from station, move through the array, until you find an object that is also in the second line array
 
 
 
 
-// for ( var i = 0; i < objKeys.length; i++ ) {
-//   var objCurrentKey = objKeys[ i ];
-//   if ( obj[ objCurrentKey ] === "Female" ) {
-//     console.log( objCurrentKey );
-//   }
+	// var twoPos = line[lineX].indexOf(stationTwo);
+	// // debugger;
+	// var smallestIndex = Math.min( onePos, twoPos );
+	// var biggestIndex = Math.max( onePos, twoPos );
+	// var betweenStations = line[lineX].slice(smallestIndex, biggestIndex);
+
+	// if (onePos > twoPos ) {
+	// 	betweenStations = betweenStations.reverse();
+	// };
+	// if ( onePos < twoPos ) {
+	// 	betweenStations = line[lineX].slice(smallestIndex+1, biggestIndex+1)
+	// };
+	// return betweenStations;
 // }
 
 
-//Object.Keys ( obj )
 
 
-// var objKeys = Object.keys( obj );
-undefined
+//The super fancy function would take stations and figure out the closest shared line.
+//This code would be incredibly clunky doing it.
+//I can almost visualise a better way of doing such a search, but haven't figured it out.
+//Can execute the code in response to the return of checkSameLine === []
+//Could search var listLine[i], for recurring loop for all positions of that
+//Then whether the corresponding object indexOfed to a station.
+//No, you need to each time check not just the current station, but then the station on either side.
+
+
 
 
 
