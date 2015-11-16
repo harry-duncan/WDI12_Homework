80402ebd0e979540dc06010bda890266b3d61157
Rhiannon Argote <rhiannon.argote@gmail.com>
2015-11-16 19:21:10 +1100

    I am obessed now

--- RhiannonArgote/week_01/Assignment_week1/mta.js
+++ RhiannonArgote/week_01/Assignment_week1/mta.js
@@ -1,7 +1,7 @@
 var lines = {
-	"N" : [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th" ],
-	"L" : ["8th", "6th", "Union Square", "3rd", "1st"],
-	"6" : ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+    "N" : [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th" ],
+    "L" : ["8th", "6th", "Union Square", "3rd", "1st"],
+    "6" : ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
 
 var goTo = function(line1, station1, line2, station2) {
@@ -16,52 +16,64 @@ var goTo = function(line1, station1, line2, station2) {
     if (line1 === line2 && entryPoint < exitPoint) {
         console.log("You must travel through the following stops on the " + line1 + ":");
         for (var i = entryPoint; i < exitPoint; i++) {
-        	var stopsPassed = startLine[i];
-        	var numStops = exitPoint - entryPoint;
+            var stopsPassed = startLine[i];
+            var numStops = exitPoint - entryPoint;
             console.log( stopsPassed );
         }
         console.log("You travelled " + numStops + " stops");
     }
 
     if (line1 === line2 && entryPoint > exitPoint) {
+         console.log("You must travel through the following stops on the " + line1 + ":");
         for (var i = entryPoint; i > exitPoint; i--) {
-        	var stopsPassed = startLine[i]
-        	var numStops = entryPoint - exitPoint;
-        	console.log("You must travel through the following stops on the " + line1 + ": "  + stopsPassed + ".");
-            console.log("You travelled " + numStops + " stops");
+            var stopsPassed = startLine[i]
+            var numStops = entryPoint - exitPoint;
+            console.log( stopsPassed );
         }
+        console.log("You travelled " + numStops + " stops");
     }
     
     if (line1 != line2 && entryPoint < unionSquare1) {
-
+ console.log("You must travel through the following stops on the " + line1 + ":");
         for (var i = entryPoint; i < unionSquare1; i++) {
             var stopsPassed = startLine[i];
             var numStops = unionSquare1 - entryPoint;
-            console.log("You must travel through " +  stopsPassed + " on the "+  line1);
+            console.log( stopsPassed );
         }
+            console.log("Change at Union Square.")
+            console.log("You travelled " + numStops + " stops."); 
     }
 
     if (line1 != line2 && entryPoint > unionSquare1) {
+     console.log("You must travel through the following stops on the " + line1 + ":");    
         for (var i = entryPoint; i > unionSquare1; i--) {
             var stopsPassed = startLine[i];
             var numStops = entryPoint - unionSquare1;
-            console.log("You must travel through stopsPassed on the "+  line1);
+            console.log( stopsPassed );
         }
+            console.log("Change at Union Square.")
+            console.log("You travelled " + numStops + " stops."); 
     }
 
     if (line1 != line2 && exitPoint < unionSquare2) {
+     console.log("You must travel through the following stops on the " + line2 + ":");    
         for (var i = entryPoint; i < unionSquare2; i++) {
             var stopsPassed = startLine[i];
             var numStops = unionSquare2 - entryPoint;
-            console.log("You must travel through stopsPassed on the "+  line1);
+            console.log( stopsPassed );
         }
+            console.log("You travelled a total of " + numStops + " stops."); 
     }
 
     if (line1 !== line2 && exitPoint > unionSquare2) {
+     console.log("You must travel through the following stops on the " + line2 + ":");    
         for (var i = exitPoint; i > unionSquare2; i--) {
             var stopsPassed = startLine[i];
             var numStops = entryPoint - unionSquare2;
-            console.log("You must travel through " + stopsPassed + " on the "+  line2);
+            console.log( stopsPassed );
         }    
+            console.log("You travelled a total of " + -(numStops) + " stops."); 
     }
 }
+
+goTo("N", "Times Square", "L", "1st");
